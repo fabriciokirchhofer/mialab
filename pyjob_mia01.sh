aa155d6c7af3f75c3a2d6a056139b366c051e9e6
Kirchhofer Fabricio (fk17u012) <fk17u012@submit04.ubelix.unibe.ch>
2024-11-01 10:03:34 +0100

    first run on UBELIX

---
+++ pyjob_mia01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name="pipeline_mia"
+#SBATCH --output=test_pipeline.txt
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:rtx3090:1  # Request one RTX 3090 GPU
+#SBATCH --time=0:30:00  # Request runtime of 1 hour
+#SBATCH --mem=64G  # Request 32GB of system memory
+#SBATCH --cpus-per-task=2  # Request 2 CPUs
+
+# Activate your conda environment
+source activate mia
+
+# Enter file to run and enter some print to ensure it ran appropriately
+python pipeline.py
