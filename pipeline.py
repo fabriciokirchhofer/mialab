3eb5e287db3862b5ffa50b1452eb38d24d432f42
Fabio <fabio.despeaux@stduents.unibe.ch>
2024-11-06 12:18:31 +0100

    added multiprocess true

--- pipeline.py
+++ pipeline.py
@@ -66,7 +66,7 @@ def main(result_dir: str, data_atlas_dir: str, data_train_dir: str, data_test_di
                           'gradient_intensity_feature': True}
 
     # load images for training and pre-process
-    images = putil.pre_process_batch(crawler.data, pre_process_params, multi_process=False)
+    images = putil.pre_process_batch(crawler.data, pre_process_params, multi_process=True)
 
     # generate feature matrix and label vector
     data_train = np.concatenate([img.feature_matrix[0] for img in images])
