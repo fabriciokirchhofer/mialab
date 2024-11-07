8a60addeb93ba8867df43348589f2dcebcd42fc9
Fabio <fabio.despeaux@stduents.unibe.ch>
2024-11-07 13:49:56 +0100

    corrected syntax tipo

--- ubelix_job_fabio.sh
+++ ubelix_job_fabio.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name="pipeline_mia"
-#SBATCH --output=test_pipeline.txt
+#SBATCH --output=doe_run.txt
 #SBATCH --time=0:30:00  # Request runtime of 1 hour
-#SBATCH --mem=48G  # Request 32GB of system memory
-#SBATCH --job-name=my_python_job      # Job name
+#SBATCH --mem=32G  # Request 32GB of system memory
 #SBATCH --ntasks=1                    # Number of tasks (1 process)
-#SBATCH --cpus-per-task=8            # CPUs per task (1 core)
+#SBATCH --cpus-per-task=6            # CPUs per task (1 core)
 
 
 # Activate your conda environment
-module load Anaconda3
-source activate mialabenv
+source activate mialab
 
 # Enter file to run and enter some print to ensure it ran appropriately
 python pipeline.py
